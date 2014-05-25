9372808fae86ca427ddea157eb4af28e40a24107
chenid0 <chenid0@gmail.com>
2014-05-25 02:20:17 -0700

    project settings and so forth

--- src/main/java/com/google/glassware/MainServlet.java
+++ src/main/java/com/google/glassware/MainServlet.java
@@ -32,12 +32,21 @@ import com.google.common.collect.Lists;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.logging.Logger;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
 
 /**
  * Handles POST requests from index.jsp
@@ -93,6 +102,30 @@ public class MainServlet extends HttpServlet {
     String userId = AuthUtil.getUserId(req);
     Credential credential = AuthUtil.newAuthorizationCodeFlow().loadCredential(userId);
     String message = "";
+    
+    Key guestbookKey = KeyFactory.createKey("Guestbook", "test");
+    String content = req.getParameter("content");
+    Date date = new Date();
+    Entity greeting = new Entity("Greeting", guestbookKey);
+    greeting.setProperty("userId", userId);
+    greeting.setProperty("credential", credential.getAccessToken());
+    greeting.setProperty("date", date);
+    greeting.setProperty("content", content);
+
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    datastore.put(greeting);
+    
+    Query q = new Query("Guestbook");
+    PreparedQuery pq = datastore.prepare(q);
+
+
+    for (Entity result : pq.asIterable()) {
+      String firstName = (String) result.getProperty("firstName");
+      String lastName = (String) result.getProperty("lastName");
+      Long height = (Long) result.getProperty("height");
+
+      System.out.println(firstName + " " + lastName + ", " + height + " inches tall");
+    }
 
     if (req.getParameter("operation").equals("insertSubscription")) {
 
